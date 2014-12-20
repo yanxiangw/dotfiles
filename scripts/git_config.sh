8c5f63cba5f8461bed17f1b69834fc75334df814
yanxiang <tauruswyx@gmail.com>
2014-12-20 13:10:27 +0800

    Fill in user's name and email when running git config

--- scripts/git_config.sh
+++ scripts/git_config.sh
@@ -8,3 +8,9 @@ git config --global alias.br branch
 git config --global alias.l "log --pretty=format:'%C(red)#%h%Creset%C(yellow)%d%Creset %C(blue)%cd%Creset %C(green)%s%Creset%C(magenta)[%cn]%Creset' --date=short --graph"
 
 git config --global color.ui true
+echo "[Enter name used for git]:"
+read name
+git config --global user.name $name
+echo "[Enter email address used for git]:"
+read email
+git config --global user.email $email
